6bfd5e5c02f2d8e0b4bffa2706368085d0b5a8cc
Artur <info@aigroupusa.com>
2021-12-01 09:31:27 -0500

    commit tree 464bc4f9e250a15bcfbc54231aced7fcbc470b1a

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(people, ivent) {
+    let cards = []
+  for (let i = 0; i < people.length; i++) {
+    cards.push(`Thank you, ${people[i]}, for the wonderful ${ivent} gift!`);
+    
+  }
+
+  return cards;
+}
+
+function countDown(num) {
+    let count = num
+while (count >= 0) {
+  console.log(count--);
+}
+}
